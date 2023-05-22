840307604e82fadf035c360ddf854760b6756856
ItsHidro <81959865+HidroGaming@users.noreply.github.com>
2023-05-22 17:43:24 +0200

    added loader

---
+++ loader.lua
@@ -0,0 +1,13 @@
+local games = {
+    [{606849621}] = "https://raw.githubusercontent.com/HidroGaming/nebulahub/main/jailbreak.lua",
+    [{6839171747}] = "https://raw.githubusercontent.com/HidroGaming/nebulahub/main/Doors.lua",
+    [{2753915549,4442272183,7449423635}] = "https://raw.githubusercontent.com/HidroGaming/nebulahub/main/bf.lua",
+    [{142823291,335132309,636649648}] = "https://raw.githubusercontent.com/HidroGaming/nebulahub/main/mm2.lua"
+}
+
+for ids, url in next, games do
+    if table.find(ids, game.PlaceId) then
+        loadstring(game:HttpGet(url))()
+        break
+    end
+end
