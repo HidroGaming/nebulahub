15dc598d98ca4accbe85e19daa1dd88fa375a9dc
ItsHidro <81959865+HidroGaming@users.noreply.github.com>
2023-05-22 17:58:27 +0200

    added petsimx to loader

--- loader.lua
+++ loader.lua
@@ -2,7 +2,8 @@ local games = {
     [{606849621}] = "https://raw.githubusercontent.com/HidroGaming/nebulahub/main/jailbreak.lua",
     [{6839171747}] = "https://raw.githubusercontent.com/HidroGaming/nebulahub/main/Doors.lua",
     [{2753915549,4442272183,7449423635}] = "https://raw.githubusercontent.com/HidroGaming/nebulahub/main/bf.lua",
-    [{142823291,335132309,636649648}] = "https://raw.githubusercontent.com/HidroGaming/nebulahub/main/mm2.lua"
+    [{142823291,335132309,636649648}] = "https://raw.githubusercontent.com/HidroGaming/nebulahub/main/mm2.lua",
+    [{6284583030}] = "https://raw.githubusercontent.com/HidroGaming/nebulahub/main/petsimx.lua",
 }
 
 for ids, url in next, games do
