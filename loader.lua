53f518475a35757f33baa73443af96851a384f5b
ItsHidro <81959865+HidroGaming@users.noreply.github.com>
2023-06-25 09:41:09 +0200

    added loader notif

--- loader.lua
+++ loader.lua
@@ -1,6 +1,9 @@
+local GameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+local Notification = loadstring(game:HttpGet("https://api.irisapp.ca/Scripts/IrisBetterNotifications.lua"))()
+
 local games = {
     [{606849621}] = "https://raw.githubusercontent.com/HidroGaming/nebulahub/main/scripts/jailbreak.lua",
-    [{6839171747}] = "https://raw.githubusercontent.com/HidroGaming/nebulahub/main/scripts/Doors.lua",
+    [{6839171747,6516141723,12308344607}] = "https://raw.githubusercontent.com/HidroGaming/nebulahub/main/scripts/Doors.lua",
     [{2753915549,4442272183,7449423635}] = "https://raw.githubusercontent.com/HidroGaming/nebulahub/main/scripts/bf.lua",
     [{142823291,335132309,636649648}] = "https://raw.githubusercontent.com/HidroGaming/nebulahub/main/scripts/mm2.lua",
     [{6284583030}] = "https://raw.githubusercontent.com/HidroGaming/nebulahub/main/scripts/petsimx.lua",
@@ -9,12 +12,15 @@ local games = {
     [{7664622578,6345118683,6379910076,12991369231,8801941581,7051262314,10428844251,9282135943,6125809531,8469430831,6372960231,7928754297}] = "https://raw.githubusercontent.com/HidroGaming/nebulahub/main/scripts/uaot.lua",
     [{7239319209,11958318242}] = "https://raw.githubusercontent.com/HidroGaming/nebulahub/main/scripts/ohio.lua",
     [{6777872443,9380307595,10184746626,12085478082,12169323719}] = "https://raw.githubusercontent.com/HidroGaming/nebulahub/main/scripts/pixelpiece.lua",
-    [{155615604}] = "https://raw.githubusercontent.com/HidroGaming/nebulahub/main/scripts/prisonlife.lua"
+    [{155615604}] = "https://raw.githubusercontent.com/HidroGaming/nebulahub/main/scripts/prisonlife.lua",
 }
 
 for ids, url in next, games do
     if table.find(ids, game.PlaceId) then
+        Notification.Notify("Nebula Hub", "Loading "..GameName,"rbxassetid://4483345998");
         loadstring(game:HttpGet(url))()
         break
+    else
+        Notification.Notify("Nebula Hub", "Game Not Found!","rbxassetid://4483345998")
     end
 end
